32b8dd328df543a9c6d6e6b64a135eafdc170890
pankhurikumar23 <pankhurikumar23@gmail.com>
2019-01-11 07:13:03 -0500

    geocoding zoom-box

--- static/js/render_map.js
+++ static/js/render_map.js
@@ -14,6 +14,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 var searchControl = L.Control.geocoder({
         defaultMarkGeocode: false
+    }).on('markgeocode', function(e) {
+        var bbox = e.geocode.bbox;
+        var poly = L.polygon([
+             bbox.getSouthEast(),
+             bbox.getNorthEast(),
+             bbox.getNorthWest(),
+             bbox.getSouthWest()
+        ]).addTo(map);
+        map.fitBounds(poly.getBounds());
     });
 
 document.getElementById('geocoder').appendChild(searchControl.onAdd(map));
