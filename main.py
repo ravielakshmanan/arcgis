6cb5045f94d4fbf5f4acf7cca1feacdbd7f58a12
Ravie Lakshmanan <ravielakshmanan@Ravies-MacBook-Air.local>
2019-01-11 10:31:47 +0530

    pushing latest changes to flask

--- main.py
+++ main.py
@@ -19,6 +19,25 @@ def get_gc_iri_data():
 
 	return downloaded_blob
 
+def find_closest_location(coords):
+	blob = get_gc_iri_data();
+
+	lng = coords.lng;
+	lat = coords.lat;
+	lng_sign = (lng < 0) ? 'W' : 'E';
+	lat_sign = (lat < 0) ? 'S' : 'N';
+	lng = (lng < 0) ? lng*(-1) : lng;
+	lat = (lat < 0) ? lat*(-1) : lat;
+
+	x = Math.round(1 + (lng - 1.25)/2.5);
+	y = Math.round(1 + (lat - 1.25)/2.5);
+
+	recreated_lng = 1.25 + (x - 1) * 2.5;
+	recreated_lat = 1.25 + (y - 1) * 2.5;
+
+
+# the format is (lat,long): {"dates":[], "data":[]}
+
 @app.route('/')
 def load_home():
 	return render_template("index.html")
@@ -31,16 +50,10 @@ def render_map():
 @app.route('/render_map/<coords>', methods=['POST'])
 def open_dataviz(coords):
 	
-	blob = get_gc_iri_data();
 
-	print(blob)
 
 	return render_template("render_map.html")
 
 if __name__ == '__main__':
    app.run(host='127.0.0.1', port=8080, debug=True)
    # app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-
-# the format is (lat,long): {"dates":[], "data":[]}
