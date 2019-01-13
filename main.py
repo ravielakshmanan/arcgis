0217ff12822f4f728ebb256849bcae57d1fb3f8f
Ravie Lakshmanan <ravielakshmanan@Ravies-MacBook-Air.local>
2019-01-13 16:21:19 +0530

    reading the json from bucket

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ from pathlib import Path
 app = Flask(__name__)
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './noah_credentials.json'
-################################################################
-#TODO: REMOVE '/' IF DOWNLOAD FILE DOES NOT WORK
-os.environ["CLOUD_STORAGE_BUCKET"] = '/water-noah.appspot.com'
-################################################################
+os.environ["CLOUD_STORAGE_BUCKET"] = 'water-noah.appspot.com'
 
 data_dictionary = 'data_dictionary.json'
-flag = ""
+status_flag = ''
+json_data = {}
 # data_dictionary = 'test.json'
 
 def get_gc_iri_data():
@@ -21,15 +19,14 @@ def get_gc_iri_data():
     bucket = gcs.get_bucket(os.environ["CLOUD_STORAGE_BUCKET"])
 
     blob = bucket.get_blob(data_dictionary)
-    blob.download_to_filename(data_dictionary)
+    # blob.download_to_filename(data_dictionary)
 
     ############################################################
     #TODO: UNCOMMENT BELOW IF DOWNLOAD OF FILE DOES NOT WORK
-    # downloaded_blob = blob.download_as_string().decode("utf-8")
-    # print(type(downloaded_blob))
-    # data = json.loads(downloaded_blob)
-    # print(type(data))
-    # return data
+    downloaded_blob = blob.download_as_string().decode("utf-8")
+    print(downloaded_blob)
+    data = json.loads(downloaded_blob)
+    return data
     ############################################################
 
 @app.route('/')
@@ -43,6 +40,8 @@ def render_map():
 
 @app.route('/onclick', methods=['GET', 'POST'])
 def open_dataviz():
+    global status_flag
+    global json_data
     lat = request.args.get('lat')
     lng = request.args.get('lng')
     latSign = request.args.get('latSign')
@@ -50,30 +49,30 @@ def open_dataviz():
 
     #############################################################
     #TODO: COMMENT BELOW IF DOWNLOAD OF FILE DOES NOT WORK
-    myFile = Path(data_dictionary)
-    if not myFile.exists():
-        print(datetime.datetime.now())
-        get_gc_iri_data()
-        print(datetime.datetime.now())
+    # myFile = Path(data_dictionary)
+    # if not myFile.exists():
+    #     print(datetime.datetime.now())
+    #     get_gc_iri_data()
+    #     print(datetime.datetime.now())
 
-    # print("File reading")
-    with open(data_dictionary) as f:
-        data = json.load(f)
-    f.close()
+    # # print("File reading")
+    # with open(data_dictionary) as f:
+    #     data = json.load(f)
+    # f.close()
     # print("File read")
     #############################################################
 
     #############################################################
     #TODO: UNCOMMENT IF DOWNLOAD OF FILE DOES NOT WORK
-    # if flag == "":
-    #     print(datetime.datetime.now())
-    #     json_data = get_gc_iri_data()
-    #     print(datetime.datetime.now())
-    # flag = "done"
-    # key = '(' + lng + lngSign + ', ' + lat + latSign + ')'
-    # print(key)
-    # if key in json_data:
-    #     return str(json_data[key])
+    if status_flag == '':
+        print(datetime.datetime.now())
+        json_data = get_gc_iri_data()
+        print(datetime.datetime.now())
+    status_flag = 'done'
+    key = '(' + lng + lngSign + ', ' + lat + latSign + ')'
+    print(key)
+    if key in json_data:
+        return str(json_data[key])
     #############################################################
 
     return "-1"
@@ -81,7 +80,3 @@ def open_dataviz():
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
     # app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-
-# the format is (long,lat): {"dates":[], "data":[]}
