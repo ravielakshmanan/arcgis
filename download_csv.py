d2640f94d0190a865b83593689bf5567387224d3
Ravie Lakshmanan <ravielakshmanan@Ravies-MacBook-Air.local>
2019-01-24 11:18:18 +0530

    updates to app.yaml

--- download_csv.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import storage
-client = storage.Client()
-bucket = client.get_bucket('magicgrant.appspot.com')
-blob = bucket.blob('transposed.csv')
-# blob = bucket.get_blob('transposed.csv')
-# downloaded_blob = blob.download_as_string()
-blob.download_to_filename('iri_data.csv')
-print('Blob {} downloaded to {}.'.format('transposed.csv','iri_data.csv'))
